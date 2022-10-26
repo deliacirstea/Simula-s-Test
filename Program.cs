2e6c8c09bc246dab769720d052e032cf34ef9e17
deliacirstea <deliutacirstea@gmail.com>
2022-10-26 09:56:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+/*Not my solution
+ * example did in class by my teacher
+ */
+
+
+ChestState currentState = ChestState.Locked;
+while (true)
+{
+    Console.WriteLine($"The chest is {currentState}. What do you want to do?");
+    string input = Console.ReadLine();
+
+    if (currentState == ChestState.Locked && input == "unlock") 
+        currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "open")
+        currentState = ChestState.Open;
+    if (currentState == ChestState.Open && input == "close")
+        currentState = ChestState.Closed;
+    if (currentState == ChestState.Closed && input == "lock")
+        currentState = ChestState.Locked;
+}
+enum ChestState { Open, Closed, Locked}
